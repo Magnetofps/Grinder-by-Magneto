52955f9adfede9db80ac7f082b3f95e65943cc94
Magnetofps <165077686+Magnetofps@users.noreply.github.com>
2024-05-13 08:35:32 +0530

    Update WeaponId.hpp

--- WeaponId.hpp
+++ WeaponId.hpp
@@ -3,40 +3,40 @@
 //constexpr int WEAPON_HANDS = 0;
 //Sniper ammo weapons
 constexpr int WEAPON_SENTINEL = 1;
-constexpr int WEAPON_CHARGE_RIFLE = 84;
-constexpr int WEAPON_LONGBOW = 86;
+constexpr int WEAPON_CHARGE_RIFLE = 87;
+constexpr int WEAPON_LONGBOW = 90;
     //Shotgun ammo weapons
-constexpr int WEAPON_MOZAMBIQUE = 98;
-constexpr int WEAPON_EVA8 = 89;
-constexpr int WEAPON_PEACEKEEPER = 105;
-constexpr int WEAPON_MASTIFF = 97;
+constexpr int WEAPON_MOZAMBIQUE = 102;
+constexpr int WEAPON_EVA8 = 93;
+constexpr int WEAPON_PEACEKEEPER = 109;
+constexpr int WEAPON_MASTIFF = 101;
     //Light ammo weapons
-constexpr int WEAPON_P2020 = 107;
-constexpr int WEAPON_RE45 = 82;
-constexpr int WEAPON_ALTERNATOR = 81;
-constexpr int WEAPON_R99 = 106;
+constexpr int WEAPON_P2020 = 111;
+constexpr int WEAPON_RE45 = 85;
+constexpr int WEAPON_ALTERNATOR = 84;
+constexpr int WEAPON_R99 = 110;
 constexpr int WEAPON_R301 = 0;
-constexpr int WEAPON_SPITFIRE = 108;
-constexpr int WEAPON_G7 = 91;
+constexpr int WEAPON_SPITFIRE = 112;
+constexpr int WEAPON_G7 = 95;
     //Heavy ammo weapons
-constexpr int WEAPON_CAR = 114;
+constexpr int WEAPON_CAR = 118;
 constexpr int WEAPON_RAMPAGE = 6;
-constexpr int WEAPON_3030 = 113;
-constexpr int WEAPON_HEMLOCK = 92;
-constexpr int WEAPON_FLATLINE = 90;
+constexpr int WEAPON_3030 = 117;
+constexpr int WEAPON_HEMLOCK = 96;
+constexpr int WEAPON_FLATLINE = 94;
     //Energy ammo weapons
-constexpr int WEAPON_NEMESIS = 115;
-constexpr int WEAPON_VOLT = 112;
-constexpr int WEAPON_TRIPLE_TAKE = 109;
-constexpr int WEAPON_LSTAR = 95;
-constexpr int WEAPON_DEVOTION = 85;
-constexpr int WEAPON_HAVOC = 87;
+constexpr int WEAPON_NEMESIS = 119;
+constexpr int WEAPON_VOLT = 116;
+constexpr int WEAPON_TRIPLE_TAKE = 113;
+constexpr int WEAPON_LSTAR = 99;
+constexpr int WEAPON_DEVOTION = 89;
+constexpr int WEAPON_HAVOC = 91;
     //Legendary ammo weapons
-constexpr int WEAPON_WINGMAN = 111;
-constexpr int WEAPON_PROWLER = 105;
+constexpr int WEAPON_WINGMAN = 114;
+constexpr int WEAPON_PROWLER = 107;
 constexpr int WEAPON_BOCEK = 2;
-constexpr int WEAPON_KRABER = 94;
-constexpr int WEAPON_THROWING_KNIFE = 169;
+constexpr int WEAPON_KRABER = 98;
+constexpr int WEAPON_THROWING_KNIFE = 173;
 constexpr int BUSTER_SWORD_R2R5 = 3;
 constexpr int WEAPON_THERMITE_GRENADE = 213;
 
